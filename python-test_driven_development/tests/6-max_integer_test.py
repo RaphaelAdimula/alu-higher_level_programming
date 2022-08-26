b924560d319b19fe27b63c9771ddd2a886844b6f
RaphaelAdimula <raphaeladimula@gmail.com>
2022-08-26 16:04:19 -0700

    5. Max integer - Unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,73 +1,19 @@
-Using ``text_indentation``
------------------------
+#!usr/bin/python3
+"""test max_integer function"""
 
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-First import ``text_indentation``
+class TestMaxInteger(unittest.TestCase):
+    """test max_integer"""
 
-    >>> text_indentation = __import__('5-text_indentation').text_indentation
-
-Now use it:
-
-Basic usage:
-    >>> text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    ... Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-    ... Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-    ... Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-    ... Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-    ... rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-    ... stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-    ... cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-    ... beatiorem! Iam ruinas videres""")
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    <BLANKLINE>
-    Quonam modo?
-    <BLANKLINE>
-    Utrum igitur tibi litteram videor an totas paginas commovere?
-    <BLANKLINE>
-    Non autem hoc:
-    <BLANKLINE>
-    igitur ne illud quidem.
-    <BLANKLINE>
-    Fortasse id optimum, sed ubi illud:
-    <BLANKLINE>
-    Plus semper voluptatis?
-    <BLANKLINE>
-    Teneo, inquit, finem illi videri nihil dolere.
-    <BLANKLINE>
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum rationi oboediens.
-    <BLANKLINE>
-    Si id dicis, vicimus.
-    <BLANKLINE>
-    Inde sermone vario sex illa a Dipylo stadia confecimus.
-    <BLANKLINE>
-    Sin aliud quid voles, postea.
-    <BLANKLINE>
-    Quae animi affectio suum cuique tribuens atque hanc, quam dico.
-    <BLANKLINE>
-    Utinam quidem dicerent alium alio beatiorem! Iam ruinas videres
-
-Non string input:
-    >>> text_indentation(3)
-    Traceback (most recent call last):
-    ...
-    TypeError: text must be a string
-
-No special char in string input:
-    >>> text_indentation("hello")
-    hello
-
-QA case:
-    >>> text_indentation("Holberton. School? How are you: John")
-    Holberton.
-    <BLANKLINE>
-    School?
-    <BLANKLINE>
-    How are you:
-    <BLANKLINE>
-    John
-
-No input:
-    >>> text_indentation()
-    Traceback (most recent call last):
-    ...
-    TypeError: text_indentation() missing 1 required positional argument: 'text'
+    def test_max_integer(self):
+        """test the function when the input is correct"""
+        self.assertAlmostEqual(max_integer([1,2,3,4]), 4)
+        self.assertAlmostEqual(max_integer([5,2,3,4]), 5)
+        self.assertAlmostEqual(max_integer([1,2, 7 ,3,4]), 7)
+        self.assertAlmostEqual(max_integer([1,2,3,-4]), 3)
+        self.assertAlmostEqual(max_integer([4]), 4)
+        self.assertAlmostEqual(max_integer([-4]), -4)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer(), None)
