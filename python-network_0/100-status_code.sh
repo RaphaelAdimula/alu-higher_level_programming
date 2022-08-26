5b4614fb48a44c72975b33eb03c97b86ee06d5c9
RaphaelAdimula <raphaeladimula@gmail.com>
2022-08-26 12:20:18 -0700

    6. Only status code

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/python3
-""" Function for peak finding """
-
-
-def find_peak(list_of_integers):
-    """
-    peak finding
-    """
-    if list_of_integers:
-        list_of_integers.sort()
-        return (list_of_integers[-1])
-    else:
-        return None
+#!/bin/bash
+# Send a request to URL and pass an argument
+curl -o /dev/null -sw "%{http_code}" "$1"
